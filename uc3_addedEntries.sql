1f13b7bc858e61e23a3c917251d3dfed444b46e2
Saipavanganesh <pavanganeshms@gmail.com>
2023-02-06 00:00:17 +0530

    added contacts

---
+++ uc3_addedEntries.sql
@@ -0,0 +1,5 @@
+INSERT INTO address_book (first_name, last_name, address, city, state, zip, phone_number, email)
+VALUES ('Sai Pavan', 'Ganesh', '1234 Main Street', 'Medchal', 'Telangana', '123456', '9876543210', 'saipavan.ganesh@email.com'),
+       ('Tony', 'Stark', '5678 Main Street', 'Houston', 'Texas', '567890', '555-555-5555', 'tony.stark@email.com'),
+       ('Bruce', 'Wayne', '9101 Main Street', 'Milan', 'Italy', '753951', '555-555-1212', 'bruce.wayne@email.com');
+
