b31504cc9af4a13a60b52092ac3720da74df3a95
Saipavanganesh <pavanganeshms@gmail.com>
2023-02-05 23:49:04 +0530

    create tables

---
+++ uc2_createTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE address_book (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(50) NOT NULL,
+  zip VARCHAR(10) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL,
+  email VARCHAR(100) NOT NULL
+);
